3e4ca2b09ea55fcf445ea1539079fb535c8282a0
Patricio Testolin <testolinp@gmail.com>
2024-04-18 17:07:51 -0300

    add ft

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -1,27 +1,28 @@
 import Link from 'next/link'
 import styles from './footer.module.css'
-import {inter} from '../fonts'
 
-export default function Footer({data}) {
-  const {mainLink, socials} = data
-
+export default function Footer() {
   return (
     <footer className={styles.footer}>
         <div className={styles.footer__wrapper}>
-          <Link href={mainLink.href} className={`${styles.footer__link} ${inter.className}`}>
-            {mainLink.title}
-          </Link>
-          <ul className={styles.footer__social}>
-            {socials.map((social, index) => {
-              const {icon, href, ariaLabel} = social
-
-              return (
-                <li className={styles.footer__social__link} key={index}>
-                  <a href={href} target='_blank' aria-label={ariaLabel} className={icon}></a>
-                </li>
-              )
-            })}
+          <div className={styles.footer__newsletter}>
+            <h2>Subscribe to <span>our newsletter</span></h2>
+            <div className={styles.footer__newsletter__form}>
+              Form
+            </div>
+          </div>
+          <ul className={styles.footer__content}>
+            <li className={styles.footer__content__address}>
+              Uriarte 1572,<br/>C1414 Cdad.<br/>Autónoma de Buenos Aires<br/>Argentina
+            </li>
+            <li className={styles.footer__content__social}><a href='http://www.twitter.com'>Twitter / X</a>
+                <br/><a href='http://www.instagram.com'>Instagram</a><br/><a href='http://www.linkedin.com'>Linkedin</a></li>
+            <li className={styles.footer__content__business}>New business<br/><a href='mailto:business@gignko.co'>business@gignko.co</a></li>
+            <li className={styles.footer__content__contact}>General enquires<br/><a href='mailto:hello@gignko.co'>hello@gignko.co</a></li>
           </ul>
+        </div>
+        <div className={styles.footer__copyright}>
+          <small>© gingko creative studio</small>
         </div>
     </footer>
   )
