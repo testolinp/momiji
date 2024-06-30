2d4f96422a719b8e409f788164dc78ecf2b77f28
Patricio Testolin <testolinp@gmail.com>
2024-06-30 14:10:41 -0300

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,15 @@ import { inter } from './ui/fonts'
 import Footer from './ui/footer/footer'
 import './ui/global.css'
 import Header from './ui/header/header'
-import { getJson } from './utils/utils'
 
 export default function RootLayout(
     { children }: { children: React.ReactNode }) {
-  const data = getJson('global.json')
-  const {footer} = data
-
   return (
     <html lang='es-AR'>
       <head>
-        <title>Sharpti</title>
+        <title>Sharpti | Transformación digital a tu medida</title>
+        <link rel='icon' type='image/x-icon' href='/images/favicon.ico' />
+        <meta name='description' content='Generamos productos digitales a medida de cada cliente, siempre con foco en la experiencia de sus usuarios' />
       </head>
       <body className={`${inter.className} antialiased`}>
         <Header></Header>
