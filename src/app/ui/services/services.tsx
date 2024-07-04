fe89d49145fdea1e9327bf1f0b6f696fc5e6f74b
Patricio Testolin <testolinp@gmail.com>
2024-07-04 10:24:07 -0300

    fix copy

--- src/app/ui/services/services.tsx
+++ src/app/ui/services/services.tsx
@@ -7,21 +7,21 @@ import { motion } from 'framer-motion'
 const services = [
   {
     title: 'Desarrollo web a medida',
-    description: 'Desde un diseño moderno y funcional hasta una experiencia de usuario excepcional, nuestro equipo experto en desarrollo web garantiza resultados sobresalientes',
-    items: ['Impulsa tu presencia digital con un sitio web personalizado que refleje la identidad única de tu marca',
-      'Consigue un sitio web responsive y optimizado para obtener mayor visibilidad en los motores de búsqueda']
+    description: 'Desde un diseño moderno y funcional hasta una experiencia de usuario excepcional, nuestro equipo experto en desarrollo web garantiza resultados sobresalientes.',
+    items: ['Impulsa tu presencia digital con un sitio web personalizado que refleje la identidad única de tu marca.',
+      'Consigue un sitio web responsive y optimizado para obtener mayor visibilidad en los motores de búsqueda.']
   },
   {
     title: 'Desarrollo de ecommerce',
-    description: 'Desde el diseño visualmente atractivo hasta la integración segura del catálogo y pasarelas de pago, te proporcionamos todo lo necesario para impulsar tus ventas en línea',
-    items: ['Convierte visitantes en clientes fieles con una tienda online intuitiva y fácil de usar', 
-      'Aumenta tus ventas con funcionalidades avanzadas como carrito abandonado, promociones exclusivas y recomendaciones personalizadas']
+    description: 'Desde el diseño visualmente atractivo hasta la integración segura del catálogo y pasarelas de pago, te proporcionamos todo lo necesario para impulsar tus ventas en línea.',
+    items: ['Convierte visitantes en clientes fieles con una tienda online intuitiva y fácil de usar.', 
+      'Aumenta tus ventas con funcionalidades avanzadas como carrito abandonado, promociones exclusivas y recomendaciones personalizadas.']
   },
   {
     title: 'Soluciones ERP',
-    description: 'Ya sea que necesites mejorar el control financiero, el inventario o los procesos logísticos, nuestro sistema ERP te brinda las herramientas para simplificar tus operaciones comerciales',
-    items: ['Automatiza tareas manuales y agiliza tus procesos empresariales clave mediante nuestra plataforma ERP personalizada', 
-      'Toma decisiones más informadas gracias al acceso instantáneo a datos precisos sobre todas las áreas funcionales de tu negocio']
+    description: 'Ya sea que necesites mejorar el control financiero, el inventario o los procesos logísticos, nuestro sistema ERP te brinda las herramientas para simplificar tus operaciones comerciales.',
+    items: ['Automatiza tareas manuales y agiliza tus procesos empresariales clave mediante nuestra plataforma ERP personalizada.', 
+      'Toma decisiones más informadas gracias al acceso instantáneo a datos precisos sobre todas las áreas funcionales de tu negocio.']
   }
 ]
 
