6cf5fb10b57085b3702987dbca4a1d28fe664c6b
Patricio Testolin <testolinp@gmail.com>
2024-07-24 16:19:37 -0300

    add mobile herospace image

--- src/app/ui/herospace/herospace.tsx
+++ src/app/ui/herospace/herospace.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 
 export default function Herospace() {
+  
   return (
     <section className={`container ${styles.herospace}`}>
       <div className={styles.herospace__content}>
@@ -16,8 +17,10 @@ export default function Herospace() {
       <div className={styles.herospace__image}>
           <motion.div className={styles.herospace__image__content} initial={{ scale: .9, opacity: 0 }}
             whileInView={{ scale: 1, opacity: 1 }} transition={{ delay: .8, duration: .5 }} viewport={{ once: true }}>
-            <Image src='/images/main.jpg' width={1600} height={0} priority={true}
-                alt='' style={{ width: '100%', height: '100%' }} />
+            <Image src='/images/herospace.jpg' width={1600} height={0} priority={true}
+                alt='' style={{ width: '100%', height: '100%' }} className={styles.desktop} />
+            <Image src='/images/herospace-mobile.jpg' width={1600} height={0} priority={true}
+                alt='' style={{ width: '100%', height: '100%' }} className={styles.mobile} />
           </motion.div>
       </div>
     </section>
